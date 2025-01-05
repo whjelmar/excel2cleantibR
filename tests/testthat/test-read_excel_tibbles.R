7b27267d4759514f2e08a28d917fd0dcd45aa524
Walter Hjelmar <walter@hjelmar.com>
2025-01-05 01:18:00 -0500

    fix: added basic excel test file

--- tests/testthat/test-read_excel_tibbles.R
+++ tests/testthat/test-read_excel_tibbles.R
@@ -1,5 +1,6 @@
 test_that("read_excel_tibbles handles basic files", {
-  file <- "tests/testthat/testdata/basic.xlsx"
+  file <- system.file("testthat/testdata/basic.xlsx", package = "excel_to_tibbles")
+  expect_true(file.exists(file)) # Ensure the file exists
   tibbles <- read_excel_tibbles(file)
   expect_type(tibbles, "list")
   expect_true(all(purrr::map_lgl(tibbles, is_tibble)))
